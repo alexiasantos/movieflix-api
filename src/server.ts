2a0bb91e6bd0662b634320f520560663cf093658
Aléxia de Oliveira <alexia.sousa@metasix.com.br>
2025-11-11 22:59:42 -0300

    correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import { PrismaClient } from '../src/generated/prisma/index.js';
 const port = 3000;
 const app = express();
 const prisma =  new PrismaClient();
+
+app.use(express.json());
 app.get("/movies", async (_, res) => {
     const movies = await prisma.movie.findMany({
         orderBy: {
@@ -15,6 +17,27 @@ app.get("/movies", async (_, res) => {
         }
     });
     res.json(movies);
+})
+
+
+app.post("/movies", async(req, res) => {
+    try {
+
+       const movieWithSameTitle = await prisma.movie.findFirst({where: {title: {equals: req.body.title, mode: 'insensitive'}}});
+        
+       if(movieWithSameTitle){
+            return res.status(409).send({ error: "A movie with the same title already exists." });
+        }
+       
+       const newMovie = await prisma.movie.create({
+            data: {
+            ...req.body
+            }
+        });
+        res.status(201).send(newMovie);
+    } catch (error) {
+        res.status(500).send({ error: "An error occurred while creating the movie." });
+    }
 })
 
 app.listen( port, () =>{
