a5a644e97f58505f5850684b3fb8aec396a7b10b
Aléxia de Oliveira <alexia.sousa@metasix.com.br>
2025-11-12 22:00:49 -0300

    Fazendo processo de build

---
+++ dist/src/generated/prisma/internal/prismaNamespaceBrowser.d.ts
@@ -0,0 +1,90 @@
+import * as runtime from "@prisma/client/runtime/index-browser";
+export type * from '../models.js';
+export type * from './prismaNamespace.js';
+export declare const Decimal: typeof runtime.Decimal;
+export declare const NullTypes: {
+    DbNull: (new (secret: never) => typeof runtime.objectEnumValues.instances.DbNull);
+    JsonNull: (new (secret: never) => typeof runtime.objectEnumValues.instances.JsonNull);
+    AnyNull: (new (secret: never) => typeof runtime.objectEnumValues.instances.AnyNull);
+};
+/**
+ * Helper for filtering JSON entries that have `null` on the database (empty on the db)
+ *
+ * @see https://www.prisma.io/docs/concepts/components/prisma-client/working-with-fields/working-with-json-fields#filtering-on-a-json-field
+ */
+export declare const DbNull: {
+    "__#private@#private": any;
+    _getNamespace(): string;
+    _getName(): string;
+    toString(): string;
+};
+/**
+ * Helper for filtering JSON entries that have JSON `null` values (not empty on the db)
+ *
+ * @see https://www.prisma.io/docs/concepts/components/prisma-client/working-with-fields/working-with-json-fields#filtering-on-a-json-field
+ */
+export declare const JsonNull: {
+    "__#private@#private": any;
+    _getNamespace(): string;
+    _getName(): string;
+    toString(): string;
+};
+/**
+ * Helper for filtering JSON entries that are `Prisma.DbNull` or `Prisma.JsonNull`
+ *
+ * @see https://www.prisma.io/docs/concepts/components/prisma-client/working-with-fields/working-with-json-fields#filtering-on-a-json-field
+ */
+export declare const AnyNull: {
+    "__#private@#private": any;
+    _getNamespace(): string;
+    _getName(): string;
+    toString(): string;
+};
+export declare const ModelName: {
+    readonly genres: "genres";
+    readonly languages: "languages";
+    readonly movies: "movies";
+};
+export type ModelName = (typeof ModelName)[keyof typeof ModelName];
+export declare const TransactionIsolationLevel: {
+    readonly ReadUncommitted: "ReadUncommitted";
+    readonly ReadCommitted: "ReadCommitted";
+    readonly RepeatableRead: "RepeatableRead";
+    readonly Serializable: "Serializable";
+};
+export type TransactionIsolationLevel = (typeof TransactionIsolationLevel)[keyof typeof TransactionIsolationLevel];
+export declare const GenresScalarFieldEnum: {
+    readonly id: "id";
+    readonly name: "name";
+};
+export type GenresScalarFieldEnum = (typeof GenresScalarFieldEnum)[keyof typeof GenresScalarFieldEnum];
+export declare const LanguagesScalarFieldEnum: {
+    readonly id: "id";
+    readonly name: "name";
+};
+export type LanguagesScalarFieldEnum = (typeof LanguagesScalarFieldEnum)[keyof typeof LanguagesScalarFieldEnum];
+export declare const MoviesScalarFieldEnum: {
+    readonly id: "id";
+    readonly title: "title";
+    readonly release_date: "release_date";
+    readonly genre_id: "genre_id";
+    readonly language_id: "language_id";
+    readonly oscar_count: "oscar_count";
+};
+export type MoviesScalarFieldEnum = (typeof MoviesScalarFieldEnum)[keyof typeof MoviesScalarFieldEnum];
+export declare const SortOrder: {
+    readonly asc: "asc";
+    readonly desc: "desc";
+};
+export type SortOrder = (typeof SortOrder)[keyof typeof SortOrder];
+export declare const QueryMode: {
+    readonly default: "default";
+    readonly insensitive: "insensitive";
+};
+export type QueryMode = (typeof QueryMode)[keyof typeof QueryMode];
+export declare const NullsOrder: {
+    readonly first: "first";
+    readonly last: "last";
+};
+export type NullsOrder = (typeof NullsOrder)[keyof typeof NullsOrder];
+//# sourceMappingURL=prismaNamespaceBrowser.d.ts.map
--- dist/src/server.js
+++ dist/src/server.js
@@ -1,22 +1,17 @@
-import express from "express"
+import express from "express";
 import { PrismaClient } from '@prisma/client';
 import swaggerUi from 'swagger-ui-express';
 import { readFileSync } from 'fs';
-
-const swaggerDocument = JSON.parse(
-  readFileSync(new URL('../swagger.json', import.meta.url), 'utf-8')
-);
-
+const swaggerDocument = JSON.parse(readFileSync(new URL('../swagger.json', import.meta.url), 'utf-8'));
 const port = 3000;
 const app = express();
-const prisma =  new PrismaClient();
-
+const prisma = new PrismaClient();
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 app.get("/movies", async (_, res) => {
     const movies = await prisma.movie.findMany({
         orderBy: {
-           title: 'asc'
+            title: 'asc'
         },
         include: {
             genres: true,
@@ -24,73 +19,62 @@ app.get("/movies", async (_, res) => {
         }
     });
     res.json(movies);
-})
-
-
-app.post("/movies", async(req, res) => {
+});
+app.post("/movies", async (req, res) => {
     try {
-
-       const movieWithSameTitle = await prisma.movie.findFirst({where: {title: {equals: req.body.title, mode: 'insensitive'}}});
-        
-       if(movieWithSameTitle){
+        const movieWithSameTitle = await prisma.movie.findFirst({ where: { title: { equals: req.body.title, mode: 'insensitive' } } });
+        if (movieWithSameTitle) {
             return res.status(409).send({ error: "A movie with the same title already exists." });
         }
-       
-       const newMovie = await prisma.movie.create({
+        const newMovie = await prisma.movie.create({
             data: {
-            ...req.body
+                ...req.body
             }
         });
         res.status(201).send(newMovie);
-    } catch (error) {
+    }
+    catch (error) {
         res.status(500).send({ error: "An error occurred while creating the movie." });
     }
-})
-
-app.put("/movies/:id", async(req, res)=>{
-    const  id  = Number(req.params.id);
-    const data = {...req.body};
+});
+app.put("/movies/:id", async (req, res) => {
+    const id = Number(req.params.id);
+    const data = { ...req.body };
     data.release_date = data.release_date ? new Date(data.release_date) : undefined;
-
-    try{
-
-        const movieExists = await prisma.movie.findUnique({where: {id}});
-        if(!movieExists){
-            return  res.status(404).send({ error: "Movie not found." });
+    try {
+        const movieExists = await prisma.movie.findUnique({ where: { id } });
+        if (!movieExists) {
+            return res.status(404).send({ error: "Movie not found." });
         }
-        const movie  = await prisma.movie.update({
-            where: {id},
+        const movie = await prisma.movie.update({
+            where: { id },
             data: {
                 ...data
             },
-        })
-
+        });
         res.json(movie);
-
-    }catch(error){
+    }
+    catch (error) {
         res.status(500).send({ error: "An error occurred while updating the movie." });
     }
-})
-
-app.delete("/movies/:id", async(req, res)=>{
-    const  id  = Number(req.params.id);
-    
-    try{
-        const movieExists = await prisma.movie.findUnique({where: {id}});
-        if(!movieExists){
-            return  res.status(404).send({ error: "Movie not found." });
+});
+app.delete("/movies/:id", async (req, res) => {
+    const id = Number(req.params.id);
+    try {
+        const movieExists = await prisma.movie.findUnique({ where: { id } });
+        if (!movieExists) {
+            return res.status(404).send({ error: "Movie not found." });
         }
         const movie = await prisma.movie.delete({
-            where:{id}
-        })
-        res.json({ message: "Movie deleted successfully."});
-
-    }catch(error){
+            where: { id }
+        });
+        res.json({ message: "Movie deleted successfully." });
+    }
+    catch (error) {
         res.status(500).send({ error: "An error occurred while deleting the movie." });
     }
-})
-
-app.get("/movies/:genreName", async(req, res) => {
+});
+app.get("/movies/:genreName", async (req, res) => {
     const genreName = req.params.genreName;
     try {
         const movies = await prisma.movie.findMany({
@@ -108,11 +92,12 @@ app.get("/movies/:genreName", async(req, res) => {
             }
         });
         res.json(movies);
-    } catch (error) {
+    }
+    catch (error) {
         res.status(500).send({ error: "An error occurred while retrieving movies by genre." });
     }
-})
-
-app.listen( port, () =>{
+});
+app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
-})
+});
+//# sourceMappingURL=server.js.map
